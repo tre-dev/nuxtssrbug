698c64a6efb24be071436e7b5f9a7ee63504e600
tre-dev <59442299+tre-dev@users.noreply.github.com>
2021-12-28 21:33:34 +0100

    init

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div style="display: flex; flex-flow: row">
+    <div>above Nuxt</div>
+    <Nuxt style="flex: 1" />
+    <div>below Nuxt</div>
+  </div>
+</template>
+
+<style scoped>
+img {
+  position: fixed;
+  top: 20px;
+  right: 20px;
+  z-index: 1000;
+}
+</style>
